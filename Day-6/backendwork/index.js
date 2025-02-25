9892682e556c3e9edd921b80fa048a96aa098056
Yogeshwaran003 <yogiwar33@gmail.com>
2025-02-25 11:27:10 +0530

    Update:FN

--- Day-6/backendwork/index.js
+++ Day-6/backendwork/index.js
@@ -1,10 +1,22 @@
 const express = require("express")
+const mdb = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config()
 const app=express()
+const port = 3000;
+
+mdb
+    .connect(process.env.MONGODB_URL)
+    .then(()=>{
+        console.log("DataBase Connected Successfully");
+    })
+    .catch((err)=>console.log("Check the connection",err))
+
 app.get('/',(req,res)=>{
     res.send("<p> Welcome to backend yogi</p>")
 })
 app.get('/static',(req,res)=>{
     res.sendFile("C:/Users/yogiw/OneDrive/Desktop/profile-modification/fullstacktraing/Yogeshwaran_Fullstack_Training/index.html")
 })
-const port = 3000;
+
 app.listen(port,()=>{console.log("Server is started")})
